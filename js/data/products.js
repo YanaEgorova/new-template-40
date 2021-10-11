929b555e6f5f437a68eb24a4f03f3be4e71fcf68
Yana <yanaegorova69.3@gmail.com>
2021-10-11 17:58:52 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,162 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Yarn Bowl',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Is it a project bag? A yarn bowl? It\'s both!',
+            },
+            {
+                text: 'Closed, your yarn and hook or needles are protected, secure and tidy, ready to be tucked away in a bag, a briefcase, or conveniently looped over your wrist.',
+            },
+            {
+                text: 'Open, it becomes a sturdy bowl to keep your yarns from twisting or rolling away as you work.',
+            },
+            {
+                text: 'Made of ultra-lightweight, flexible silicon, there is nothing to snag or damage your project.',
+            },
+            {
+                text: 'Available in a choice of three jewel shades, with a whimsical design of knitting stitches, this innovative project caddy takes your knit and crochet experience to the next level.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Leg Cast Cover',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Ideal for the WATERTIGHT PROTECTION of casts, bandages, rashes, abrasions, burns, cuts, wounds, IV & PICC Lines.',
+            },
+            {
+                text: 'REUSABLE and QUALITY made. The cast cover is made of a heavy duty vinyl that can be reused for approximately 6-8 weeks.',
+            },
+            {
+                text: 'EASY APPLICATION. It is easy to put on, just slide it up and over your foot until the seal makes contact with your skin.',
+            },
+            {
+                text: 'Designed to fit EVERYONE in the family. Unique diaphragm allows for custom, secure fit around your leg.',
+            },
+            {
+                text: 'Made with NON-LATEX materials. The "seal" is made from a non-latex material and stretches easy to accommodates most size legs and casts.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: '6-Light Chandelier',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Classic Meets Contemporary',
+                text: 'Classic white shades get a glossy upgrade for a contemporary feel that pairs perfectly with the dramatic dark bronze frame.',
+            },
+            {
+                title: 'Versatile Placement',
+                text: 'Includes 4 stems of varying lengths for a range of hanging heights; 12. 61" minimum, 52. 42" maximum length. The pivot point below the canopy makes this fixture easy to hang anywhere, even sloped ceilings.',
+            },
+            {
+                title: 'Dimmable',
+                text: 'Completely dimmable with a compatible dimmer switch to create the perfect ambience.',
+            },
+            {
+                title: 'Easy-To-Install',
+                text: 'All mounting hardware is included for quick and easy installation.',
+            },
+            {
+                title: 'Bulb Information',
+                text: 'Requires six 60W E26 bulbs or equivalents (sold separately).',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Moisture Meter',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Measures',
+                text: 'Separate settings for drywall, masonry, softwood and hardwood.',
+            },
+            {
+                title: 'No Drywall Damage',
+                text: 'Non-marring sensing technology protects drywall.',
+            },
+            {
+                title: 'Easy To Read',
+                text: 'LEDs clearly indicate moisture level.',
+            },
+            {
+                title: 'HI Visibility Screen',
+                text: 'Large 2 inch (50mm) backlit white on black screen providing easier readability from any angle.',
+            },
+            {
+                title: 'Find Mold',
+                text: 'Stops mold before it starts with early detection of moisture; essential water damage tool.',
+            },
+            {
+                title: 'Ergonomic',
+                text: 'Improved ergonomic handle design conforms to your hand for a more comfortable user experience.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Portable Folding Camp Kitchen',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Folding Camping Table',
+                text: 'Folding camp table brings the kitchen outside with enough counter and storage space to accommodate camp kitchen equipment like stoves and grills, cooking utensils, and cookware.',
+            },
+            {
+                title: 'Quality Construction',
+                text: '1-piece powder-coated steel outdoor table frame easily unfolds to reveal an aluminum countertop, storage rack, 3 fold-out side tables, collapsible sink, and lantern pole.',
+            },
+            {
+                title: 'Specialized Shelving',
+                text: 'Outdoor prep table features heat-resistant countertop (48-lb. weight limit) for camp stoves, a lower rack (35-lb. weight capacity) for dry good storage, and a collapsible softshell sink with drain for dishes.',
+            },
+            {
+                title: 'Spacious Storage',
+                text: 'Foldable table features 3 fold-out side tables with space for food and beverages, a paper towel holder, and hooks for grilling utensils and garbage bags.',
+            },
+            {
+                title: 'Compact',
+                text: 'Portable table with carry handles folds flat for storage; Measures 21.7 x 56.3 x 32.3 inches open, 5.9 x 21.7 x 34.8 inches folded, and weighs 22.7 pounds.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'ADF Helmet',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Adjustable ratchet head band to custom fit your head and absorbent.',
+            },
+            {
+                text: 'Oversize sweatband for improved comfort.',
+            },
+            {
+                text: 'Processes stick, tig, pulsed tig, mig, pulsed mig, flux-cored, gouging.',
+            },
+            {
+                text: 'Lightweight helmet makes for ease of use.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+]
